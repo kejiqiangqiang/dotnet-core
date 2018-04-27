c7fa107734b161b88741feceeb44c2fcebafc9b6
caiq <974897633@qq.com>
2018-04-27 17:12:08 +0800

    Initial commit

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.ReadKey();
+        }
+        
+    }
+}
